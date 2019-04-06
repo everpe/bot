0e4bd16285f8ccc05b350ea6f0fbd8c89c98bbb0
ever <ever.penab@autonoma.edu.co>
2019-04-06 11:05:41 -0500

    se agregó ayuda

--- routes/botman.php
+++ routes/botman.php
@@ -8,8 +8,28 @@ $botman->hears('Hi  ', function ($bot) {
 });
 $botman->hears('/start', function ($bot) {
     $nombres=$bot->getUser()->getFirtName()?:"desconocido";
-    $bot->reply ('hola'.$nombres.'bienvenido al bot quizzes'
-    
+    $bot->reply ('hola'.$nombres.'bienvenido al bot quizzes');
 });
 
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->fallback(function($bot){
+    $bot->reply("no entiendoooo");
+});
+
+
+$botman->hears('/ayuda', function ($bot) {
+	$ayuda = ['/ayuda' => 'Mostrar este mensaje de ayuda',
+          	'acerca de|acerca' => 'Ver la información quien desarrollo este lindo bot',
+          	'listar quizzes|listar' => 'Listar los cuestionarios disponibles',
+          	'iniciar quiz <id>' => 'Iniciar la solución de un cuestionario',
+          	'ver puntajes|puntajes' => 'Ver los últimos puntajes',
+          	'borrar mis datos|borrar' => 'Borrar mis datos del sistema'];
+    
+	$bot->reply("Los comandos disponibles son:");
+
+	foreach($ayuda as $key=>$value)
+	{
+    		$bot->reply($key . ": " . $value);
+	}
+});
